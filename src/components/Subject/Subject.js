0c4a997d286667a74c518213992f25289efccccc
d38u6 <d38u6dev@gmail.com>
2020-07-01 14:01:24 +0200

    Refactorin and add theme handling to Category component

--- src/components/Subject/Subject.js
+++ src/components/Subject/Subject.js
@@ -1,50 +1,42 @@
 import React from "react";
+import PropTypes from "prop-types";
+
 import { Col, Card } from "react-bootstrap";
 
-import image from "../../assets/image/pro.jpg";
-import image2 from "../../assets/image/pro2.jpg";
+import defaultImage from "../../assets/image/defaultImage.jpg";
+import withTheme from "../../HOC/withTheme";
 
-const Subject = () => (
+const Subject = ({ imageURL, title, desc, lastUpdate, theme }) => (
   <>
     <Col md="4">
-      <Card>
+      <Card bg={theme.bg} text={theme.text}>
         <Card.Img
           variant="top"
-          src={image}
+          src={imageURL || defaultImage}
           style={{ height: "160px", objectFit: "cover" }}
         />
         <Card.Body>
-          <Card.Title>Card title</Card.Title>
-          <Card.Text>
-            This card has supporting text below as a natural lead-in to additional
-            content.
-          </Card.Text>
+          <Card.Title>{title}</Card.Title>
+          <Card.Text>{desc}</Card.Text>
         </Card.Body>
         <Card.Footer>
-          <small className="text-muted">Last updated 3 mins ago</small>
-        </Card.Footer>
-      </Card>
-    </Col>
-    <Col md="4">
-      <Card>
-        <Card.Img
-          variant="top"
-          src={image2}
-          style={{ height: "160px", objectFit: "cover" }}
-        />
-        <Card.Body>
-          <Card.Title>Card title</Card.Title>
-          <Card.Text>
-            This card has supporting text below as a natural lead-in to additional
-            content.
-          </Card.Text>
-        </Card.Body>
-        <Card.Footer>
-          <small className="text-muted">Last updated 3 mins ago</small>
+          <small className="text-muted">{lastUpdate}</small>
         </Card.Footer>
       </Card>
     </Col>
   </>
 );
 
-export default Subject;
+Subject.propTypes = {
+  imageURL: PropTypes.string,
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string,
+  lastUpdate: PropTypes.number, //UNIX TIME STAMP
+  //theme Context
+  theme: PropTypes.shape({
+    bg: PropTypes.oneOf(["dark", "light"]),
+    text: PropTypes.oneOf(["dark", "light"]),
+  }),
+};
+
+export default withTheme(Subject);
