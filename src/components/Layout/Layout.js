d5a82c82b736ada4cf9fa52acd0c309ad22b2e81
d38u6 <d38u6dev@gmail.com>
2020-07-01 12:35:06 +0200

    Replace Container with Content in Layout

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,12 @@
 import React from "react";
-import { Container } from "react-bootstrap";
+
 import Header from "./Header/Header";
+import Content from "./Content/Content";
 
 const Layout = ({ children }) => (
   <>
     <Header />
-    <Container className="content">{children}</Container>
+    <Content>{children}</Content>
   </>
 );
 
