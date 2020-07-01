97ed328d814439ced14b8944dbb3e3df6b1b3338
d38u6 <d38u6dev@gmail.com>
2020-07-01 13:09:10 +0200

    Refactoring 'Header' and add theme handling

--- src/components/Layout/Header/Header.js
+++ src/components/Layout/Header/Header.js
@@ -1,16 +1,29 @@
-import React, { useContext } from "react";
+import React from "react";
+import PropTypes from "prop-types";
+
 import { Navbar } from "react-bootstrap";
 import ToggleThemeButton from "../../Theme/ToggleThemeButton";
-import ThemeContext from "../../Theme/ThemeContext";
+import withTheme from "../../../HOC/withTheme";
 
-const Header = () => {
-  const { theme } = useContext(ThemeContext);
+const Header = ({ theme }) => {
   return (
-    <Navbar bg={theme} variant={theme} className="justify-content-between">
+    <Navbar
+      bg={theme.bg}
+      variant={theme.bg}
+      className="justify-content-between"
+    >
       <Navbar.Brand href="/">Progres Tracker</Navbar.Brand>
       <ToggleThemeButton />
     </Navbar>
   );
 };
 
-export default Header;
+Header.propTypes = {
+  //theme Context
+  theme: PropTypes.shape({
+    bg: PropTypes.oneOf(["dark", "light"]),
+    text: PropTypes.oneOf(["dark", "light"]),
+  }),
+};
+
+export default withTheme(Header);
