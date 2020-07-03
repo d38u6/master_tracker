47bd89bb6875b6cf797379b75395e8c41b93d886
d38u6 <d38u6dev@gmail.com>
2020-07-03 10:59:18 +0200

    Added Edit button to CategoryBox

--- src/components/CategoryBox/CategoryBox.js
+++ src/components/CategoryBox/CategoryBox.js
@@ -5,33 +5,47 @@ import { Col, Card } from "react-bootstrap";
 import classes from "./CategoryBox.module.css";
 import defaultImage from "../../assets/image/defaultImage.jpg";
 import withTheme from "../../HOC/withTheme";
+import EditButton from "../Utility/EditButton/EditButton";
 
-const CategoryBox = ({ imageURL, title, desc, lastUpdate, theme }) => (
-  <Col md="4">
-    <div className={classes.CategoryBox}>
-      <Card bg={theme.bg} text={theme.text}>
-        <Card.Img
-          variant="top"
-          src={imageURL || defaultImage}
-          style={{ height: "160px", objectFit: "cover" }}
-        />
-        <Card.Body>
-          <Card.Title>{title}</Card.Title>
-          <Card.Text>{desc}</Card.Text>
-        </Card.Body>
-        <Card.Footer>
-          <small className="text-muted">{lastUpdate}</small>
-        </Card.Footer>
-      </Card>
-    </div>
-  </Col>
-);
+const CategoryBox = ({
+  imageURL,
+  title,
+  desc,
+  lastUpdate,
+  onEditClick,
+  theme,
+}) => {
+  return (
+    <Col md="4">
+      <div className={classes.CategoryBox}>
+        <div className={classes.EditButton}>
+          <EditButton onClick={onEditClick} />
+        </div>
+        <Card bg={theme.bg} text={theme.text}>
+          <Card.Img
+            variant="top"
+            src={imageURL || defaultImage}
+            style={{ height: "160px", objectFit: "cover" }}
+          />
+          <Card.Body>
+            <Card.Title>{title}</Card.Title>
+            <Card.Text>{desc}</Card.Text>
+          </Card.Body>
+          <Card.Footer>
+            <small className="text-muted">{lastUpdate}</small>
+          </Card.Footer>
+        </Card>
+      </div>
+    </Col>
+  );
+};
 
 CategoryBox.propTypes = {
   imageURL: PropTypes.string,
   title: PropTypes.string.isRequired,
   description: PropTypes.string,
   lastUpdate: PropTypes.number, //UNIX TIME STAMP
+  onEditClick: PropTypes.func,
   //theme Context
   theme: PropTypes.shape({
     bg: PropTypes.oneOf(["dark", "light"]),
