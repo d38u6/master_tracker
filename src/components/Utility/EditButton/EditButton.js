b2f2588a6756b5990f6cf22f2789517c4c8daf9e
d38u6 <d38u6dev@gmail.com>
2020-07-03 10:20:02 +0200

    Added EditButton utility component

---
+++ src/components/Utility/EditButton/EditButton.js
@@ -0,0 +1,19 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { FaPencilAlt } from "react-icons/fa";
+
+import classes from "./EditButton.module.css";
+
+const EditButton = ({ onClick }) => {
+  return (
+    <div className={classes.EditButton} onClick={onClick}>
+      <FaPencilAlt />
+    </div>
+  );
+};
+
+EditButton.propTypes = {
+  onClick: PropTypes.func,
+};
+
+export default EditButton;
