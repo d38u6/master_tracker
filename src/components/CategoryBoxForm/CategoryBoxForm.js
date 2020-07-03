8fe12824b683252ee98061850904cc2843686d73
d38u6 <d38u6dev@gmail.com>
2020-07-03 13:26:06 +0200

    Added CategoryBoxForm component

---
+++ src/components/CategoryBoxForm/CategoryBoxForm.js
@@ -0,0 +1,70 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Col, Card, Form } from "react-bootstrap";
+
+import withTheme from "../../HOC/withTheme";
+import ApplyButton from "../Utility/ApplyButton/ApplyButton";
+
+import classes from "./CategoryBoxForm.module.css";
+import defaultImage from "../../assets/image/defaultImage.jpg";
+
+const CategoryBoxForm = ({ titleConf, descConf, onSave, theme }) => {
+  return (
+    <Col md="4">
+      <div className={classes.CategoryBox}>
+        <div className={classes.ApplyButton}>
+          <ApplyButton onClick={onSave} />
+        </div>
+        <Card bg={theme.bg} text={theme.text}>
+          <Card.Img
+            variant="top"
+            src={defaultImage}
+            style={{ height: "160px", objectFit: "cover" }}
+          />
+          <Card.Body>
+            <Card.Title>
+              <Form.Control
+                className={classes.TitleInput}
+                type="text"
+                placeholder="Category TItle"
+                value={titleConf.value}
+                onChange={titleConf.onChange}
+              />
+            </Card.Title>
+            <Card.Text>
+              <Form.Control
+                as="textarea"
+                rows="3"
+                placeholder="Category description"
+                value={descConf.value}
+                onChange={descConf.onChange}
+              />
+            </Card.Text>
+          </Card.Body>
+          <Card.Footer>
+            <small className="text-muted">not yet</small>
+          </Card.Footer>
+        </Card>
+      </div>
+    </Col>
+  );
+};
+
+CategoryBoxForm.propTypes = {
+  titleConf: PropTypes.shape({
+    value: PropTypes.string,
+    onChange: PropTypes.func.isRequired,
+  }),
+  descConf: PropTypes.shape({
+    value: PropTypes.string,
+    onChange: PropTypes.func.isRequired,
+  }),
+  onSave: PropTypes.func,
+  //theme Context
+  theme: PropTypes.shape({
+    bg: PropTypes.oneOf(["dark", "light"]),
+    text: PropTypes.oneOf(["dark", "light"]),
+  }),
+};
+
+export default withTheme(CategoryBoxForm);
