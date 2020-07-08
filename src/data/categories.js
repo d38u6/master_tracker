41716c4cbaece670776a40b5c61f743f2d6a13af
d38u6 <d38u6dev@gmail.com>
2020-07-08 10:58:45 +0200

    added initial categories array

---
+++ src/data/categories.js
@@ -0,0 +1,10 @@
+import defaultImage from "../assets/image/categories/default.jpg";
+export const initialCategories = [
+  {
+    id: 0,
+    imageSrc: defaultImage,
+    title: "Your First Category",
+    desc: "This is your first example category",
+    time: 0,
+  },
+];
