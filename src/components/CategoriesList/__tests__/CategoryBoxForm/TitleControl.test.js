f69d876c727ea43a9ec3448d5904835545255996
d38u6 <d38u6dev@gmail.com>
2020-07-11 20:07:47 +0200

    Added tests to 'TitleControl component'

---
+++ src/components/CategoriesList/__tests__/CategoryBoxForm/TitleControl.test.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { shallow } from "enzyme";
+
+import TitleControl from "../../CategoryBoxForm/TitleControl/TitleControl";
+import { titleConf } from "../../../../data/fixtures";
+
+const props = { ...titleConf };
+
+describe("'TitleControl' component", () => {
+  const titleControl = shallow(<TitleControl {...props} />);
+
+  //CardTitle
+  it("render 'CardTitle' component", () => {
+    expect(titleControl.find("CardTitle").exists()).toBe(true);
+  });
+
+  //FormControl
+  it("render 'FormControl' component", () => {
+    expect(titleControl.find("FormControl").exists()).toBe(true);
+  });
+
+  it("'FormControl' should contain proper props", () => {
+    expect(titleControl.find("FormControl").props()).toMatchObject(props);
+  });
+
+  it("should call 'onChange' callback with new value", () => {
+    const newValue = "new test title value";
+    titleControl.find("FormControl").simulate("change", newValue);
+
+    expect(props.onChange).toHaveBeenCalledWith(newValue);
+  });
+});
