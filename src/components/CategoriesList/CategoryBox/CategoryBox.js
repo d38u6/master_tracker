7d912f96fd22cf731062717758c4804fab2f8ccd
d38u6 <d38u6dev@gmail.com>
2020-07-11 18:26:59 +0200

    Refactor 'CategoryBox' to smaller components - Use 'CategoryBoxWrapper' and 'CategoryImage' components

--- src/components/CategoriesList/CategoryBox/CategoryBox.js
+++ src/components/CategoriesList/CategoryBox/CategoryBox.js
@@ -1,36 +1,29 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { Col, Card } from "react-bootstrap";
+import { Card } from "react-bootstrap";
 
-import CardWithTheme from "../../Utility/CardWithTheme/CardWithTheme";
+import CategoryBoxWrapper from "./CategoryBoxWrapper/CategoryBoxWrapper";
 import EditButton from "./EditButton/EditButton";
+import CardWithTheme from "../../Utility/CardWithTheme/CardWithTheme";
+import CategoryImage from "./CategoryImage/CategoryImage";
 
 import classes from "./CategoryBox.module.css";
 
 function CategoryBox({ imageSrc, title, desc, time, onEditClick }) {
   return (
-    <Col md="4">
-      <div className={classes.CategoryBox}>
-        <EditButton className={classes.EditButton} onClick={onEditClick} />
-        {/* <div className={classes.EditButton}>
-          <EditButton onClick={onEditClick} />
-        </div> */}
-        <CardWithTheme>
-          <Card.Img
-            variant="top"
-            src={imageSrc}
-            style={{ height: "160px", objectFit: "cover" }}
-          />
-          <Card.Body>
-            <Card.Title>{title}</Card.Title>
-            <Card.Text>{desc}</Card.Text>
-          </Card.Body>
-          <Card.Footer>
-            <small className="text-muted">{time}</small>
-          </Card.Footer>
-        </CardWithTheme>
-      </div>
-    </Col>
+    <CategoryBoxWrapper className={classes.CategoryBox}>
+      <EditButton className={classes.EditButton} onClick={onEditClick} />
+      <CardWithTheme>
+        <CategoryImage src={imageSrc} />
+        <Card.Body>
+          <Card.Title>{title}</Card.Title>
+          <Card.Text>{desc}</Card.Text>
+        </Card.Body>
+        <Card.Footer>
+          <small className="text-muted">{time}</small>
+        </Card.Footer>
+      </CardWithTheme>
+    </CategoryBoxWrapper>
   );
 }
 
