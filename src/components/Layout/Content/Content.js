2d9d7f25190bb3b70fe043bedb72b4bb0ab2e8be
d38u6 <d38u6dev@gmail.com>
2020-07-12 15:19:30 +0200

    Little refactor and added tests to 'Content' component

--- src/components/Layout/Content/Content.js
+++ src/components/Layout/Content/Content.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { Container } from "react-bootstrap";
+import classes from "./Content.module.css";
 
 function Content({ children }) {
-  return <Container className="content">{children}</Container>;
+  return <Container className={classes.Content}>{children}</Container>;
 }
 
 export default Content;
