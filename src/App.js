c770a10d45b062452c49091a7797a01bf76cf8a8
d38u6 <d38u6dev@gmail.com>
2020-07-18 20:38:12 +0200

    Create 'Category' component and add routing to him

--- src/App.js
+++ src/App.js
@@ -3,27 +3,32 @@ import { Route } from "react-router-dom";
 
 import Theme from "./components/Theme/Theme";
 import Layout from "./components/Layout/Layout";
-import CategoriesList from "./components/CategoriesList/CategoriesList";
 import CategoriesListContainer from "./containers/CategoriesList/CategoriesListContainer";
+import CategoriesList from "./components/CategoriesList/CategoriesList";
+import Category from "./components/Category/Category";
+
+const categories = (
+  <Route
+    path="/"
+    exact
+    render={() => (
+      <CategoriesListContainer
+        render={({ categories, addCategory }) => (
+          <CategoriesList categories={categories} onAddClick={addCategory} />
+        )}
+      />
+    )}
+  />
+);
+
+const category = <Route path="/category" exact render={() => <Category />} />;
 
 function App() {
   return (
     <Theme>
       <Layout>
-        <Route
-          path="/"
-          exact
-          render={() => (
-            <CategoriesListContainer
-              render={({ categories, addCategory }) => (
-                <CategoriesList
-                  categories={categories}
-                  onAddClick={addCategory}
-                />
-              )}
-            />
-          )}
-        />
+        {categories}
+        {category}
       </Layout>
     </Theme>
   );
