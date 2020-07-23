0c2ce3577c57b2e238d63e2a292cd27a1c9331c1
d38u6 <d38u6dev@gmail.com>
2020-07-23 12:05:22 +0200

    Added tests to 'AddButton' SubjectsList component

---
+++ src/components/Category/__tests__/SubjectsList/AddButton.test.js
@@ -0,0 +1,58 @@
+import React from "react";
+import { shallow } from "enzyme";
+
+import AddButton from "../../SubjectsList/AddButton/AddButton";
+
+const props = { onClick: jest.fn() };
+
+describe("'AddButton' component", () => {
+  const addButton = shallow(<AddButton {...props} />);
+
+  //tr
+  it("render 'tr' element", () => {
+    expect(addButton.find("tr").exists()).toBe(true);
+  });
+
+  //td
+  it("render 'td' element", () => {
+    expect(addButton.find("td").exists()).toBe(true);
+  });
+
+  it("'td' element should contain proper props", () => {
+    const desiredProps = { colSpan: "4", className: "Wrapper" };
+    expect(addButton.find("td").props()).toMatchObject(desiredProps);
+  });
+
+  //Button
+  it("render 'Button' component", () => {
+    expect(addButton.find("Button").exists()).toBe(true);
+  });
+
+  it("Button' component should contain proper props", () => {
+    const desiredProps = {
+      variant: "success",
+      size: "sm",
+      className: "AddButton",
+      onClick: props.onClick,
+    };
+    expect(addButton.find("Button").props()).toMatchObject(desiredProps);
+  });
+
+  it("'Button' component should contain 'Add New' text", () => {
+    expect(addButton.find("Button").text()).toMatch("Add New");
+  });
+
+  // Button > BsPlus
+  it("inside 'Button' should render 'BsPlus' icon", () => {
+    expect(addButton.find("Button > BsPlus").exists()).toBe(true);
+  });
+
+  it("'BsPlus' should contain 'Icon' class", () => {
+    expect(addButton.find("Button > BsPlus").hasClass("Icon")).toBe(true);
+  });
+
+  it("should call onClick callback", () => {
+    addButton.find("Button").simulate("click");
+    expect(props.onClick).toHaveBeenCalled();
+  });
+});
