d2402be84228ed84e3808ee10d1d82f5dd6db0a4
d38u6 <d38u6dev@gmail.com>
2020-07-23 13:57:00 +0200

    Added tests to 'EditButton' SubjectRow

---
+++ src/components/Category/__tests__/SubjectsList/SubjectRow/EditButton.test.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { shallow } from "enzyme";
+
+import EditButton from "../../../SubjectsList/SubjectRow/EditButton/EditButton";
+
+const props = { onClick: jest.fn() };
+
+describe("'EditButton' component", () => {
+  const editButton = shallow(<EditButton {...props} />);
+
+  //td
+  it("render 'td' element", () => {
+    expect(editButton.find("td").exists()).toBe(true);
+  });
+
+  it("'td' element should contain 'EditButton' class", () => {
+    expect(editButton.find("td").hasClass("EditButton")).toBe(true);
+  });
+
+  //div
+  it("render 'div' element", () => {
+    expect(editButton.find("div").exists()).toBe(true);
+  });
+
+  it("'div' element should contain proper props", () => {
+    const desiredProps = { className: "Button", onClick: props.onClick };
+    expect(editButton.find("div").props()).toMatchObject(desiredProps);
+  });
+
+  //FaPencilAlt icon
+  it("render 'FaPencilAlt' icon inside 'div'", () => {
+    expect(editButton.find("div > FaPencilAlt").exists()).toBe(true);
+  });
+});
