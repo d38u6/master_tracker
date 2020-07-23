c75eb34fad950f3464e5e8ab8351ba553876baa1
d38u6 <d38u6dev@gmail.com>
2020-07-23 11:17:41 +0200

    Added tests to 'SubjectsList' component

---
+++ src/components/Category/__tests__/SubjectsList/SubjectsList.test.js
@@ -0,0 +1,60 @@
+import React from "react";
+import { shallow } from "enzyme";
+
+import SubjectsList from "../../SubjectsList/SubjectsList";
+import { subjects } from "../../../../data/fixtures";
+
+const props = { subjects, onAddClick: jest.fn() };
+
+describe("'SubjectsList' component", () => {
+  const subjectsList = shallow(<SubjectsList {...props} />);
+
+  //TabelWithTheme
+  it("render 'TabeleWithTheme' component", () => {
+    expect(subjectsList.find("WithTheme(TableWithTheme)").exists()).toBe(true);
+  });
+
+  it("'TabelWithTheme' should contain propre props", () => {
+    const desiredProps = {
+      className: "SubjectsList",
+      striped: true,
+      hover: true,
+      size: "sm",
+    };
+    expect(
+      subjectsList.find("WithTheme(TableWithTheme)").props()
+    ).toMatchObject(desiredProps);
+  });
+
+  //tbody
+  it("render 'tbody' element", () => {
+    expect(
+      subjectsList.find("WithTheme(TableWithTheme) > tbody").exists()
+    ).toBe(true);
+  });
+
+  //SubjectRowEditable
+  it("render correct number of 'SubjectRowEditable' components", () => {
+    expect(subjectsList.find("Memo(SubjectRowEditable)").length).toBe(
+      props.subjects.length
+    );
+  });
+
+  subjectsList
+    .find("Memo(SubjectRowEditable)")
+    .forEach((subjectRowEditable, i) => {
+      it(`'SubjectRowEditable' ${i} should contain proper props`, () => {
+        expect(subjectRowEditable.props()).toMatchObject(props.subjects[i]);
+      });
+    });
+
+  //AddButton
+  it("render 'AddButton' component", () => {
+    expect(subjectsList.find("AddButton").exists()).toBe(true);
+  });
+
+  it("should call 'onAddClick' callback", () => {
+    subjectsList.find("AddButton").simulate("click");
+    expect(props.onAddClick).toHaveBeenCalled();
+  });
+});
