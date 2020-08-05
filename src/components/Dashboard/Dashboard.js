9a32cf35d715c73541500b088c178d268278e816
d38u6 <d38u6dev@gmail.com>
2020-08-05 13:53:23 +0200

    Added styles to 'Dashborad' component

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,14 +1,18 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { Row } from "react-bootstrap";
+import classes from "./Dashboard.module.css";
 
 import GoalChartContainer from "../../containers/GoalChart/GoalChartContainer";
 import Widget from "../Utility/Widget/Widget";
 import CircleProgressBar from "../Utility/CircleProgressBar/CircleProgressBar";
+import ChartWithTheme from "../Utility/ChartWithTheme/ChartWithTheme";
+import TimeChartContainer from "../../containers/TimeChart/TimeChartContainer";
+import ChartWrapper from "../ChartWrapper/ChartWrapper";
 
 function Dashboard({ records }) {
   return (
-    <Row>
+    <Row className={classes.Dashboard}>
       <GoalChartContainer
         records={records}
         render={({ name, menuItems, selectItem, progressBarConf }) => (
@@ -19,6 +23,17 @@ function Dashboard({ records }) {
               onSelect={selectItem}
             ></Widget.ContextMenu>
             <CircleProgressBar {...progressBarConf} />
+          </Widget>
+        )}
+      />
+
+      <TimeChartContainer
+        records={records}
+        render={({ chartConf }) => (
+          <Widget md="6" name="Last 7 days">
+            <ChartWrapper>
+              <ChartWithTheme {...chartConf} />
+            </ChartWrapper>
           </Widget>
         )}
       />
