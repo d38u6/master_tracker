b1200b2b0130ada3b0e479f00554f86720fc7315
d38u6 <d38u6dev@gmail.com>
2020-08-06 12:34:25 +0200

    Added tests to 'ChartWrapper' component

--- src/components/ChartWrapper/ChartWrapper.js
+++ src/components/ChartWrapper/ChartWrapper.js
@@ -2,11 +2,16 @@ import React from "react";
 import PropTypes from "prop-types";
 import classes from "./ChartWrapper.module.css";
 
-function ChartWrapper({ children }) {
-  return <div className={classes.Wrapper}>{children}</div>;
+function ChartWrapper({ className, children }) {
+  return (
+    <div className={[className || "", classes.Wrapper].join(" ")}>
+      {children}
+    </div>
+  );
 }
 
 ChartWrapper.propTypes = {
+  className: PropTypes.string,
   children: PropTypes.element,
 };
 
