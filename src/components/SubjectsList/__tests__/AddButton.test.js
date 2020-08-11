5f402416ace78eb25a1ce3cc797e1dfed5d79c54
d38u6 <d38u6dev@gmail.com>
2020-08-11 13:15:01 +0200

    Refactoring 'Subjects list AddButton' component to using 'RowButton' component

--- src/components/SubjectsList/__tests__/AddButton.test.js
+++ src/components/SubjectsList/__tests__/AddButton.test.js
@@ -8,51 +8,39 @@ const props = { onClick: jest.fn() };
 describe("'AddButton' component", () => {
   const addButton = shallow(<AddButton {...props} />);
 
-  //tr
-  it("render 'tr' element", () => {
-    expect(addButton.find("tr").exists()).toBe(true);
+  //RowButton
+  it("render 'RowButton' component", () => {
+    expect(addButton.find("RowButton").exists()).toBe(true);
   });
 
-  //td
-  it("render 'td' element", () => {
-    expect(addButton.find("td").exists()).toBe(true);
-  });
-
-  it("'td' element should contain proper props", () => {
-    const desiredProps = { colSpan: "4", className: "Wrapper" };
-    expect(addButton.find("td").props()).toMatchObject(desiredProps);
-  });
-
-  //Button
-  it("render 'Button' component", () => {
-    expect(addButton.find("Button").exists()).toBe(true);
-  });
-
-  it("Button' component should contain proper props", () => {
+  it("RowButton' component should contain proper props", () => {
     const desiredProps = {
       variant: "success",
-      size: "sm",
-      className: "AddButton",
       onClick: props.onClick,
     };
-    expect(addButton.find("Button").props()).toMatchObject(desiredProps);
+    expect(addButton.find("RowButton").props()).toMatchObject(desiredProps);
   });
 
-  it("'Button' component should contain 'Add New' text", () => {
-    expect(addButton.find("Button").text()).toMatch("Add New");
+  //span
+  it("render 'span' element", () => {
+    expect(addButton.find("span").exists()).toBe(true);
+  });
+
+  it("'span' element should contain 'Add New' text", () => {
+    expect(addButton.find("span").text()).toBe("Add New");
   });
 
   // Button > BsPlus
-  it("inside 'Button' should render 'BsPlus' icon", () => {
-    expect(addButton.find("Button > BsPlus").exists()).toBe(true);
+  it("inside 'RowButton' should render 'BsPlus' icon", () => {
+    expect(addButton.find("RowButton > BsPlus").exists()).toBe(true);
   });
 
   it("'BsPlus' should contain 'Icon' class", () => {
-    expect(addButton.find("Button > BsPlus").hasClass("Icon")).toBe(true);
+    expect(addButton.find("RowButton > BsPlus").hasClass("Icon")).toBe(true);
   });
 
   it("should call onClick callback", () => {
-    addButton.find("Button").simulate("click");
+    addButton.find("RowButton").simulate("click");
     expect(props.onClick).toHaveBeenCalled();
   });
 });
