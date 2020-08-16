ddd26ff20510ab3c3c8cdb81659f74fda3429588
d38u6 <d38u6dev@gmail.com>
2020-08-16 13:46:56 +0200

    Create 'AlertCategoryRemoveConfirm' component

---
+++ src/components/Alerts/AlertCategoryRemoveConfirm.js
@@ -0,0 +1,31 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import ConfirmModal from "../Utility/ConfirmModal/ConfirmModal";
+
+function AlertCategoryRemoveConfirm({ onClose, onRemove }) {
+  return (
+    <ConfirmModal
+      title="Remove Category"
+      body="All yours data belonging to this category, like subjects and records will be permanently removed. Are you sure?"
+      options={[
+        { caption: "Close", variant: "secondary", onClick: onClose },
+        {
+          caption: "Remove",
+          variant: "danger",
+          onClick: () => {
+            onRemove();
+            onClose();
+          },
+        },
+      ]}
+    />
+  );
+}
+
+AlertCategoryRemoveConfirm.propTypes = {
+  onClose: PropTypes.func.isRequired,
+  onRemove: PropTypes.func.isRequired,
+};
+
+export default AlertCategoryRemoveConfirm;
--- src/components/Alerts/index.js
+++ src/components/Alerts/index.js
@@ -1,6 +1,9 @@
 import AlertChangesSaved from "./AlertChangesSaved";
 import AlertNewCategoryAdded from "./AlertNewCategoryAdded";
+import AlertCategoryRemoveConfirm from "./AlertCategoryRemoveConfirm";
+
 export default {
   ChangesSaved: AlertChangesSaved,
   NewCategoryAdded: AlertNewCategoryAdded,
+  CategoryRemoveConfirm: AlertCategoryRemoveConfirm,
 };
