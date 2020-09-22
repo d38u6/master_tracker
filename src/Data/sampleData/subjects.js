0cbd9d58a9b0848338c4788a406fad2387aff3d0
d38u6 <d38u6dev@gmail.com>
2020-09-22 14:49:14 +0200

    Added position property to sample subjects

--- src/Data/sampleData/subjects.js
+++ src/Data/sampleData/subjects.js
@@ -1,19 +1,19 @@
 export default [
   //coding
-  { id: "bvo9KP6fr", categoryId: "Fg5MnuKIC", title: "Mastert Tracker" },
-  { id: "ixoqIi7tE", categoryId: "Fg5MnuKIC", title: "Mario in JS" },
-  { id: "I4daxzVPX", categoryId: "Fg5MnuKIC", title: "Courses" },
-  { id: "-Xbx3-QbS", categoryId: "Fg5MnuKIC", title: "Barber Website" },
+  { id: "bvo9KP6fr", categoryId: "Fg5MnuKIC", title: "Mastert Tracker", position: 1 },
+  { id: "ixoqIi7tE", categoryId: "Fg5MnuKIC", title: "Mario in JS", position: 2 },
+  { id: "I4daxzVPX", categoryId: "Fg5MnuKIC", title: "Courses", position: 3 },
+  { id: "-Xbx3-QbS", categoryId: "Fg5MnuKIC", title: "Barber Website", position: 4 },
 
   //school
-  { id: "Z2XmI9u7K", categoryId: "ZJzrCEIXl", title: "English" },
-  { id: "B-rfQH5Ay", categoryId: "ZJzrCEIXl", title: "Math" },
-  { id: "wGTNKFE_A", categoryId: "ZJzrCEIXl", title: "History" },
-  { id: "BVB3bRxJj", categoryId: "ZJzrCEIXl", title: "Geography" },
-  { id: "pwUb1dGB5", categoryId: "ZJzrCEIXl", title: "Physics" },
+  { id: "Z2XmI9u7K", categoryId: "ZJzrCEIXl", title: "English", position: 1 },
+  { id: "B-rfQH5Ay", categoryId: "ZJzrCEIXl", title: "Math", position: 2 },
+  { id: "wGTNKFE_A", categoryId: "ZJzrCEIXl", title: "History", position: 3 },
+  { id: "BVB3bRxJj", categoryId: "ZJzrCEIXl", title: "Geography", position: 4 },
+  { id: "pwUb1dGB5", categoryId: "ZJzrCEIXl", title: "Physics",position: 5 },
 
   //soccer
-  { id: "hxCvP1B_a", categoryId: "siKuHjdFM", title: "Matches" },
-  { id: "WnxP_PEhe", categoryId: "siKuHjdFM", title: "Free kick" },
-  { id: "FarkQgn2b", categoryId: "siKuHjdFM", title: "Tactics" },
+  { id: "hxCvP1B_a", categoryId: "siKuHjdFM", title: "Matches", position: 1 },
+  { id: "WnxP_PEhe", categoryId: "siKuHjdFM", title: "Free kick", position: 2 },
+  { id: "FarkQgn2b", categoryId: "siKuHjdFM", title: "Tactics", position: 3 },
 ];
